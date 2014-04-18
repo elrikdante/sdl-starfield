bb6876c130c0b2594f852773af684158d7510a90
Hashrocket Workstation <dev@hashrocket.com>
2014-04-18 14:47:08 -0400

    Them Object files tho

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <SDL/SDL.h>
-#define NUM_DOTS 1024
+#define NUM_DOTS 10192
+#define SCREEN_WIDTH 	640
+#define WINDOW_TITLE "Unicorn Tears: Star Field X"
+#define SCREEN_HEIGHT	480
 
 struct dot
 {
-	int red,green,blue;
+	int red,green,blue,speed;
 	float vx, vy;
 	float x,y;	
 };
@@ -19,22 +22,27 @@ void init_dots() {
 		demo_dots[i].red = rand() % 255;
 		demo_dots[i].green = rand() % 255;
 		demo_dots[i].blue = rand() % 255;
-		demo_dots[i].x = rand() % 320;
-		demo_dots[i].y = rand() % 240;
+		demo_dots[i].x = rand() % SCREEN_WIDTH;
+		demo_dots[i].y = rand() % SCREEN_HEIGHT;
+		demo_dots[i].speed = 1 + rand() % 5;
 	}
 }
 
 void move_dots() {
+	int i = 0;
+	for (i; i < NUM_DOTS; i ++) {
+		demo_dots[i].x = ((int)(demo_dots[i].x + demo_dots[i].speed) % 640);
+	}
 }
 
 void draw_screen() {
 	int i=0,bpp,rank,x,y;
 	Uint32 *pixel;
-	SDL_LockSurface(main_screen);
 	rank = main_screen->pitch/sizeof(Uint32);
 	pixel = (Uint32*)main_screen->pixels;
 
-	for(; i < NUM_DOTS ; i++) {
+	SDL_LockSurface(main_screen);
+	for(i=0; i < NUM_DOTS ; i++) {
 		x = (int)demo_dots[i].x;
 		y = (int)demo_dots[i].y;
 		pixel[x+y*rank] = SDL_MapRGBA(
@@ -51,25 +59,36 @@ void draw_screen() {
 
 int main(int argn, char **argv) {
 	int active;
+	int gFrames = 0;
+
 	SDL_Event event;
 
 	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
 		fprintf(stderr, "Could not initialise %s\n",SDL_GetError());
 	}
 
-	main_screen = SDL_SetVideoMode(320,240,0,SDL_HWSURFACE|SDL_DOUBLEBUF);
+	main_screen = SDL_SetVideoMode(
+		SCREEN_WIDTH,
+		SCREEN_HEIGHT,
+		0,
+		SDL_HWSURFACE|SDL_DOUBLEBUF
+	);
 
 	if (!main_screen) {
 		fprintf(stderr, "Could not initialise %s\n", SDL_GetError());
 	}
+
+	SDL_WM_SetCaption( WINDOW_TITLE, 0 );
+
 	printf("SDL On Line\n");
 	init_dots();	
 	active = 1;
+
 	while(active) {
 		SDL_FillRect(main_screen,NULL,SDL_MapRGBA(main_screen->format,0,0,255,255));
-		move_dots();
 		draw_screen();
 		SDL_Flip(main_screen);
+		move_dots();
 		while(SDL_PollEvent(&event)) {
 			if (event.type == SDL_QUIT) {
 				active = 0;
